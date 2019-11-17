7b4c27520cc5414e096776ab2bd51ce6e58fc734
evilcloud <43460664+evilcloud@users.noreply.github.com>
2019-11-17 13:05:43 +0800

    accidental explanation

--- temp.py
+++
@@ -1,9 +0,0 @@
-def solve(meal_cost, tip_percent, tax_percent):
-    # totalCost = int(meal_cost + (meal_cost*(tip_percent/100)) + (meal_cost*(tax_percent/100)))
-    tip_amount = meal_cost * (tip_percent / 100)
-    tax_percent = meal_cost * (tax_percent / 100)
-    totalCost = meal_cost + tip_amount + tax_percent
-    
-    print(round(totalCost))
-    
-solve(10.25, 17, 5)
