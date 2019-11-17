b94ff4b9eb1e09d955e004ed58f91f3913aa161f
kgr0 <mrgkail@gmail.com>
2019-11-18 07:04:36 +0800

    still working on file functions

--- brut.py
+++ brut.py
@@ -17,11 +17,13 @@ def save_txt(line):
         text_file.write(line + "\n")
     return
 
+
 def save_git_md(line):
     file_name = "gmd" + file_name_component + ".md"
     with open(file_name, "a") as gitmd_file:
         gitmd_file.write(line + "<br/>")
     return
+
 
 def save_vanila_md(line):
     file_name = "vmd" + file_name_component + ".md"
@@ -29,24 +31,28 @@ def save_vanila_md(line):
         vanmd_file.write(line + "\n")
     return
 
+
 def register_timedata():
     file_name = "tds" + file_name_component + ".ser"
     with open(file_name, "a") as time_file:
         time_file.write(str(datetime.timestamp(datetime.now())) + "\n")
     return
 
+
 def save_oversharing(line):
     file_name = "ooo" + file_name_component + ".md"
     with open(file_name, "a") as overshare:
         overshare.write("    " + line + "<br/>")
-        overshare.write(humanise_timedelta(datetime.now()-start_time))
+        overshare.write(humanise_timedelta(datetime.now() - start_time))
     return
+
 
 # TO BE COMPLETED TOMORROW
 def saving_process(final_line, file_name):
     with open(file_name, "a") as target_file:
         target_file.write(final_line)
     return
+
 
 def save_to_all_files(line):
     save_txt(line)
@@ -55,7 +61,8 @@ def save_to_all_files(line):
     save_oversharing(line)
     register_timedata()
     return
-    
+
+
 def save_except_txt(line):
     save_txt("")
     save_git_md(line)
@@ -64,16 +71,19 @@ def save_except_txt(line):
     register_timedata()
     return
 
+
 # SYSTEM
 def quit_app():
-    save_to_all_files("⌹")
+    save_except_txt("⌹")
     print("   " + session_duration())
     print("\nbye")
     quit()
 
+
 def clear_screen():
     _ = call("cls" if osname == "nt" else "clear")
     return
+
 
 # MENU
 def help_screen():
@@ -110,8 +120,8 @@ def menu(line):
     elif line in stats_command:
         stat_screen()
     else:
-        return True # was a true line, not a command
-    return False # this was a command line
+        return True  # was a true line, not a command
+    return False  # this was a command line
 
 
 # DATAPROC
@@ -140,10 +150,11 @@ def session_duration():
 
 def first_line(check_file):
     if os.path.exists(check_file):
-        opening_line = "⌸" + " "*15 + str(start_time.time().replace(microsecond=0))
+        opening_line = "⌸" + " " * 15 + str(start_time.time().replace(microsecond=0))
     else:
         opening_line = "⌷   " + str(start_time.replace(microsecond=0))
     return opening_line
+
 
 # MAIN
 # INITIALIZE
