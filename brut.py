8ee8e679a0e7410193d0fba749df26f135a3b92c
evilcloud <mrgkail@gmail.com>
2019-11-20 13:23:40 +0800

    removing 4f and replacing with CSV - last chance to see old sys

--- brut.py
+++ brut.py
@@ -3,19 +3,42 @@ from os import name as osname
 from os import path as ospath
 import os.path
 from datetime import date, datetime
+import csv
 
 # CONFIG
-global start_time
-global file_name_component
-start_time = datetime.now()
-file_name_component = str(start_time.date())
+global session_start_time
+global session_date
+session_start_time = datetime.now()
+session_date = str(session_start_time.date())
 
 # FILE FUNCTIONS
 def save_txt(line):
-    file_name = "bas" + file_name_component + ".txt"
+    file_name = "bas" + session_date + ".txt"
     saving_process(line + "\n", file_name)
     return
 
+
+# ANOTHER FILESYSTEM
+# THE FILES, AS BEFORE, WOULD BE ISSUED DAILY, BECAUSE WRITING IS TO BE SPLIT INTO GEOTEMPORAL REALITY
+# THIS MEANS THAT ANY WRITING COMMITED AT AND AFTER 00:00 OF THE NEXT DAY, EVEN IF SESSION STARTED THE DAY BEFORE,
+# WILL CREATE AND SAVE INTO THE NEW-DAY'S FILE. THIS IS A CONTIOUS DESIGN DECISION, BASED ON THE PHILOSOPHY
+# OF THE PROJECT
+
+# FILE FORMAT: CSV, TWO COLLUMNS, COMMA SEPARATED
+# FIRST VALUE: TIMESTAMP. SECOND VALUE: STRING.
+# SESSION START WILL HAVE SPECIAL SESSION START CHAR IN THE TIMESTAMP FIELD AND TIMESTAMP IN STRING
+def save_line(line):
+    file_name = str(session_date) + ".bru"
+    with open(file_name, "a") as file:
+        writer = csv.writer(file)
+        writer.writerow(line)
+    return
+
+
+#####################################
+# ALL OF THIS IS ABOUT TO GET DELETED
+# AS WE ARE MOVING TO CSV FROM 4F
+#  ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼
 
 # WORKING ON THIS RIGHT NOW
 def temporary_function():
@@ -27,37 +50,37 @@ def temporary_function():
             "ooo",
             ".md",
             line
-            + f"<br/>    {(humanise_timedelta(datetime.now() - start_time))}<br/><br/>",
+            + f"<br/>    {(humanise_timedelta(datetime.now() - session_start_time))}<br/><br/>",
         ],
     }
 
 
 def save_git_md(line):
-    file_name = "gmd" + file_name_component + ".md"
+    file_name = "gmd" + session_date + ".md"
     saving_process(line + "<br/>", file_name)
     return
 
 
 def save_vanila_md(line):
-    file_name = "vmd" + file_name_component + ".md"
+    file_name = "vmd" + session_date + ".md"
     final_line = line + "\n"
     saving_process(final_line, file_name)
     return
 
 
 def register_timedata():
-    file_name = "tds" + file_name_component + ".ser"
+    file_name = "tds" + session_date + ".ser"
     final_line = str(datetime.timestamp(datetime.now())) + "\n"
     saving_process(final_line, file_name)
     return
 
 
 def save_oversharing(line):
-    file_name = "ooo" + file_name_component + ".md"
+    file_name = "ooo" + session_date + ".md"
     final_line = (
         line
         + "<br>    "
-        + (humanise_timedelta(datetime.now() - start_time) + "<br/><br/>")
+        + (humanise_timedelta(datetime.now() - session_start_time) + "<br/><br/>")
     )
     saving_process(final_line, file_name)
     return
@@ -85,6 +108,9 @@ def save_except_txt(line):
     save_oversharing(line)
     register_timedata()
     return
+
+# THE END OF THE ABOUT-TO-BE-DELETED SECION
+###################################
 
 
 # SYSTEM
@@ -160,27 +186,40 @@ def humanise_timedelta(td):
 def session_duration():
     """Returns [str] "session duration: [huminized time]
     gets data from [huminise_timedelta]"""
-    sess_dur = datetime.now() - start_time
+    sess_dur = datetime.now() - session_start_time
     return "session duration: " + humanise_timedelta(sess_dur)
 
 
 def first_line(check_file):
     if os.path.exists(check_file):
-        opening_line = "⌸" + " " * 15 + str(start_time.time().replace(microsecond=0))
+        opening_line = (
+            "⌸" + " " * 15 + str(session_start_time.time().replace(microsecond=0))
+        )
     else:
-        opening_line = "⌷   " + str(start_time.replace(microsecond=0))
+        opening_line = "⌷   " + str(session_start_time.replace(microsecond=0))
     return opening_line
 
 
 # MAIN
 # INITIALIZE
 clear_screen()
-save_except_txt(first_line("tds" + file_name_component + ".ser"))
+
+############################ ABOUT TO BE GONE
+save_except_txt(first_line("tds" + session_date + ".ser"))
 save_to_all_files("")
+##############################
+
+# NEW FILE FORMAT PROC - ABOUT TO BE SET AS DEFAULT
+save_line(["⌸", str(datetime.timestamp(datetime.now()))])
 
 # TEXT ENTRY
 while True:
     line = input("    ")
     print("\n\n")
     if menu(line):
+        ############### BE GONE
         save_to_all_files(line)
+        
+        # NEW FILE FORMAT -- TO BE SET AS DEFAULT
+        csv_line = [str(datetime.timestamp(datetime.now())), line]
+        save_line(csv_line)
